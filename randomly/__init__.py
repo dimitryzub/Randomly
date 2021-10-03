f7a153211a1aafb5c8d4e4089c7a42a08b701524
Dimitry Zub <dimitryzub@gmail.com>
2021-10-03 13:53:51 +0300

    changed info

--- randomly/__init__.py
+++ randomly/__init__.py
@@ -1,10 +1,11 @@
 '''Python library to generate random things'''
-
 from .random_words import Words
 
 __all__ = [
     'Words',
 ]
 
-
 __version__ = "0.1.0"
+
+
+
--- randomly/__version__.py
+++
@@ -1,3 +0,0 @@
-'''Python library to generate random things'''
-
-__version__ = "0.1.0"
